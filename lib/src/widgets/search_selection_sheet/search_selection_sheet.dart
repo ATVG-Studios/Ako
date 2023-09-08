bd58e371e109b23cdc4aecf6da525d0f69da69c1
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2023-09-08 11:51:05 +0200

    Run dart-format

--- lib/src/widgets/search_selection_sheet/search_selection_sheet.dart
+++ lib/src/widgets/search_selection_sheet/search_selection_sheet.dart
@@ -83,7 +83,7 @@ class AkoSearchSelectionStyle {
     this.bottomButtonPadding = const EdgeInsets.all(8.0),
     this.bottomButtonSeparator = const BorderSide(color: Colors.grey, width: 1),
     this.bottomSheetShape = const RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
+      borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
     ),
     this.clearButtonStyle,
     this.submitButtonStyle,
