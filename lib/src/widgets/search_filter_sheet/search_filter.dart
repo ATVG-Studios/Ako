a330bc9bac810a20e0f48c074966ed9ec8ab3eaf
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2023-10-19 11:20:04 +0200

    Add copyright header to search_filter_sheet files

--- lib/src/widgets/search_filter_sheet/search_filter.dart
+++ lib/src/widgets/search_filter_sheet/search_filter.dart
@@ -1,3 +1,8 @@
+// Copyright 2023 the Ako authors (https://pub.dev/packages/ako).
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import 'package:flutter/material.dart';
 
 import 'search_filter_options.dart';
