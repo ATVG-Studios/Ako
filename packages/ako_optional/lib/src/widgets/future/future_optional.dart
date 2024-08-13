5ebefdc67b93e65aeccefb2f0907d0f6012d3145
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2024-08-13 14:37:03 +0200

    misc: Update copyright notices

--- packages/ako_optional/lib/src/widgets/future/future_optional.dart
+++ packages/ako_optional/lib/src/widgets/future/future_optional.dart
@@ -1,3 +1,8 @@
+// Copyright 2023-2024 the Ako authors (https://pub.dev/packages/ako).
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import 'package:ako_core/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:optional/optional_internal.dart';
